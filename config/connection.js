8bb7698cc10f197e4b243639cb5f46889bfc5af3
saraalinazari <sara.alinazari@gmail.com>
2018-06-06 19:06:53 -0700

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,27 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Mnis!103666",
-  database: "burgers_db"
-});
-
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Mnis!103666",
+//   database: "burgers_db"
+// });
+if (process.env.JAWSDB_URL) {
+  // DB is JawsDB on Heroku
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  // DB is local on localhost
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Mnis!103666",
+    database: "burgers_db"
+  });
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
